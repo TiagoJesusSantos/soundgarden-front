692300be588d6fd2400fa307b6075f257f44fcf3
Tiago <zoonbrasil@gmail.com>
2023-03-16 20:26:35 -0300

    listar evento

---
+++ js/listar.js
@@ -0,0 +1,11 @@
+const eventList = () => { 
+    return fetch('https://soundgarden-api.vercel.app//events').then(
+        (response) => {
+            if (response.ok) { 
+                console.log(response.json())
+                return response.json();
+            }  
+            throw new Error("nao foi possivel listar os eventos");      
+        }
+    )
+}
